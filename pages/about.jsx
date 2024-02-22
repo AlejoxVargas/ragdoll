4be0b31f016e9a565dd98ee8667ee9f23a1e861d
AliciaoE <alialioh@gmail.com>
2024-02-22 04:22:11 +0100

    Agregando archivos nuevos

---
+++ pages/about.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+
+export default function Home() {
+  return (
+    
+    <div>
+      <Navbar/>
+     
+      
+      <Footer/>
+    </div>
+  
+  );
+}
---
+++ pages/prueba/prueba.jsx
@@ -0,0 +1,13 @@
+// pages/prueba/prueba.jsx
+import React from 'react';
+
+const PruebaPage = () => {
+  return (
+    <div>
+      <h1>Página de Prueba</h1>
+      <p>Este es un ejemplo de una página de prueba en Next.js</p>
+    </div>
+  );
+};
+
+export default PruebaPage;
